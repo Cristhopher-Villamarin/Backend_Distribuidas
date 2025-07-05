2f18ddcc70046cd4f39bfa8fa68e7a23b7f10585
Cristhopher-Villamarin <87491312+Cristhopher-Villamarin@users.noreply.github.com>
2025-07-05 15:56:11 -0500

    Update error merge

--- eventos/src/config/index.js
+++ eventos/src/config/index.js
@@ -24,8 +24,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Conexión a la base de datos establecida');
-
+    console.log('Conexión a la base de datos establecida');S
   } catch (error) {
     console.error('No se pudo conectar a la base de datos:', error);
     process.exit(1);
